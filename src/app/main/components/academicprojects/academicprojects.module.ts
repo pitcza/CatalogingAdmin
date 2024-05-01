f662f1ce552a62bd56adaff51fb22e58b683348f
Czarina Jane Arellano <czarinaarellano12@gmail.com>
2024-05-02 01:24:41 +0800

    inayos styles (colors lang)

--- src/app/main/components/academicprojects/academicprojects.module.ts
+++ src/app/main/components/academicprojects/academicprojects.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AcademicprojectsRoutingModule } from './academicprojects-routing.module';
 
 import { ListofprojectsComponent } from './components/listofprojects/listofprojects.component';
 import { AddprojectComponent } from './components/addproject/addproject.component';
-import { EditdetailsComponent } from './components/editdetails/editdetails.component';
 
 // POP UPS
 import { CancelPopupComponent } from './components/cancel-popup/cancel-popup.component';
 import { DeletePopupComponent } from './components/delete-popup/delete-popup.component';
+import { EditdetailsComponent } from './components/editdetails/editdetails.component';
 import { DetailsPopupComponent } from './components/details-popup/details-popup.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AddprojectComponent,
     EditdetailsComponent,
-    CancelPopupComponent,
-    DeletePopupComponent,
     DetailsPopupComponent
   ],
   imports: [
